3a06186f1bca26a83564f9889b0b11b7cb41f055
Tremaine Scott <Trezzy52@gmail.com>
2019-02-01 20:11:08 -0500

    I created seperate classes for my test cases from UserTests, and added 1 class to smoke_tests

--- src/test/java/com/BriteErp/tests/CreateEventFunctionality/Tremaine/functional_tests/BRIT_201.java
+++ src/test/java/com/BriteErp/tests/CreateEventFunctionality/Tremaine/functional_tests/BRIT_201.java
@@ -6,50 +6,10 @@ import org.testng.annotations.Test;
 
 
 
-public class UserTestCases extends TestBase {
+public class BRIT_201 extends TestBase {
 
     @Test
-    public void BRIT_109() {
-        extentLogger = report.createTest("Create Event_BRIT_109 Test-User");
-        waitForPageToLoad(2);
-
-        extentLogger.info("1.Precondition: Environment is ready.");
-        driver.get(ConfigurationReader.getProperties("url"));
-        driver.manage().window().maximize();
-
-        extentLogger.info("2.Go to landing page.");
-        extentLogger.info("Expected Result: User is taken to landing page");
-        pages.landing().BriteErpDemo_button.click();
-
-        extentLogger.info("3.Login as a User.");
-        extentLogger.info("Expected Result: User enters correct credentials and is taken to homepage");
-        pages.login().userLogin();
-
-        extentLogger.info("4.User then clicks on Calendar tab");
-        extentLogger.info("Expected Result: User is taken to the Calendar page.");
-        pages.topNavigationBar().calendar_button.click();
-
-        extentLogger.info("5.User click on the Day button for main Calendar.");
-        extentLogger.info("Expected Result: Calendar is refreshed to its Day format.");
-        pages.calendar().viewDay_button.click();
-
-        extentLogger.info("6.User clicks on All-Day box");
-        extentLogger.info("Expected Result: User should be presented with a pop-up box");
-        pages.calendar().allDayBox.click();
-
-        extentLogger.info("7.User enters text to the summary input box");
-        pages.calendar().summaryInputBox.sendKeys("Testing Purposes");
-
-        extentLogger.info("8.User then clicks on create button");
-        extentLogger.info("Expected Result: User should have successfully created an event");
-        pages.calendar().popOutCreate_button.click();
-
-
-        //verify that user has successfully created an event
-    }
-
-    @Test
-    public void BRIT_201(){
+    public void BRIT_201test(){
         extentLogger = report.createTest("Add attendee's to event_BRIT_201 Test-User");
         waitForPageToLoad(2);
 
