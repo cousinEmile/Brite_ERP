d36081994f9f9cf68808bec968fe814999fcac53
Feyyaz <mfeyyazcimen@gmail.com>
2019-02-02 20:49:29 -0500

    after fixed

--- src/test/java/com/BriteErp/tests/CreateEventFunctionality/mFeyyaz/smoke_tests/BRIT_333_CreateAnEvent.java
+++ src/test/java/com/BriteErp/tests/CreateEventFunctionality/mFeyyaz/smoke_tests/BRIT_333_CreateAnEvent.java
@@ -1,7 +1,6 @@
 package com.BriteErp.tests.CreateEventFunctionality.mFeyyaz.smoke_tests;
 
 import com.BriteErp.utilities.TestBase;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
@@ -14,43 +13,48 @@ public class BRIT_333_CreateAnEvent extends TestBase {
     @Test
     public void CreateAnEventTest() {
 
-//         openCalenderMontlyView();
         pages.login().open();
+
         pages.landing().BriteErpDemo_button.click();
+
         pages.login().userLogin();
+        wait(2);
+
         pages.topNavigationBar().calendar_button.click();
+
+        wait(2);
         pages.calendar().viewMonth_button.click();
 
-         extentLogger = report.createTest("BRIT_333_CreateAnEvent Test");
+        extentLogger = report.createTest("BRIT_333_CreateAnEvent Test");
 
-         extentLogger.info("1. Pre-Condition: The Calender page should be displayed in monthly view");
-         Assert.assertTrue( pages.calendar().monthView.isDisplayed() );
+        extentLogger.info("1. Pre-Condition: The Calender page should be displayed in monthly view");
+        Assert.assertTrue( pages.calendar().monthView.isDisplayed() );
 
-         extentLogger.info("2. Select a month by clicking on right or left arrow.");
-         extentLogger.info("Expected: Months should change in order");
+        extentLogger.info("2. Select a month by clicking on right or left arrow.");
+        extentLogger.info("Expected: Months should change in order");
 
-         extentLogger.info("3. Come to the date May, 2019.");
-         extentLogger.info("Expected: The days of May 2019 should be displayed");
+        extentLogger.info("3. Come to the date May, 2019.");
+        extentLogger.info("Expected: The days of May 2019 should be displayed");
 
-         CurrentMonth =  goToThatMonth("May");
-         Assert.assertTrue(CurrentMonth.contains("May"));
+        CurrentMonth =  goToThatMonth("May");
+        Assert.assertTrue(CurrentMonth.contains("May"));
 
-         extentLogger.info("4. Click on day 15th.");
-         extentLogger.info("Expected: Create window should pop-up");
-         pages.calendar().may15th.click();
+        extentLogger.info("4. Click on day 15th.");
+        extentLogger.info("Expected: Create window should pop-up");
+        pages.calendar().may15th.click();
 
-         wait(2);
-         extentLogger.info("5. Create an event named: test event May15 2019");
-         extentLogger.info("Expected: The event should appear");
-         pages.calendar().summaryInputBox.sendKeys(eventName);
+        wait(2);
+        extentLogger.info("5. Create an event named: test event May15 2019");
+        extentLogger.info("Expected: The event should appear");
+        pages.calendar().summaryInputBox.sendKeys(eventName);
 
-         pages.calendar().popOutCreate_button.click();
-         wait(4);
+        pages.calendar().popOutCreate_button.click();
+        wait(4);
 
-         Assert.assertEquals(pages.calendar().getEvent(eventName).getText().trim(),eventName.trim());
+        Assert.assertEquals(pages.calendar().getEvent(eventName).getText().trim(),eventName.trim());
 
 
-         extentLogger.pass("CreateAnEventTest");
+        extentLogger.pass("CreateAnEventTest");
     }
 
     private String eventName = "test event May15 2019";
@@ -83,8 +87,6 @@ public class BRIT_333_CreateAnEvent extends TestBase {
         }
         return null;
     }
-
-
 
 
 }
