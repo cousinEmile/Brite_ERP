2a61efa4c3f1768587efa1b6782bc25d67fe32c8
AimanG <gainedenova2@gmail.com>
2019-02-09 16:06:05 -0500

    exp result

--- src/test/java/com/BriteErp/tests/ImportFunctionality/Aiman/smoke_tests/BRIT_243_RepeatUntil.java
+++ src/test/java/com/BriteErp/tests/ImportFunctionality/Aiman/smoke_tests/BRIT_243_RepeatUntil.java
@@ -16,37 +16,45 @@ public class BRIT_243_RepeatUntil extends TestBase {
 
         extentLogger.info("1.Go to the URL");
         driver.get(ConfigurationReader.getProperties("url"));
+        extentLogger.info("Expected Result: System should display the website");
 
         extentLogger.info("2.Click on BriteErpDemo");
         pages.landing().BriteErpDemo_button.click();
+        extentLogger.info("Expected Result: System should navigate to login page.");
 
         extentLogger.info("3.Enter email and password for Inventory User 4 and click Log in");
         pages.login().userLogin();
         waitForPageToLoad(3);
+        extentLogger.info("Expected Result: System should accept username and password and navigate to home page");
 
         extentLogger.info("4.Click on Calendar tab on the top");
         pages.topNavigationBar().calendar_button.click();
+        extentLogger.info("Expected Result: Calendar opens");
 
         extentLogger.info("5.Click on List button on the right, at the top of the mini-Calendar");
         pages.calendar().listView.click();
+        extentLogger.info("Expected Result: List opens.");
 
         extentLogger.info("6.Click on Import icon");
         pages.calendarListViewPage().import_button.click();
+        extentLogger.info("Expected Result: Import opens");
 
         extentLogger.info("7.Click on Load File and select a CSV or Excel file to import");
         WebElement chooseFile = pages.importPage().loadFile;
         String file = "/Users/aimangainedenova/Downloads/Utility.xlsx";
         chooseFile.sendKeys(file);
+        extentLogger.info("Expected Result: The file should be printed and displayed on the screen");
 
         wait(2);
         extentLogger.info("8. Unselect 'Show fields of relation fields (advanced)'");
         pages.importPage().showFields.click();
+        extentLogger.info("Expected Result: Unselected");
 
         extentLogger.info("9. Click 'Repeat Until' on the selective panel ");
-
         pages.importPage().dontImportField.click();
         WebElement end = pages.importPage().search;
         end.sendKeys("Repeat Until" + Keys.ENTER);
+        extentLogger.info("Expected Result: 'Repeat Until'  is  selected");
 
         extentLogger.pass("Repeat Until");
 
